d6e4eddd3ec733d57125331643c77a96dc0a5ad3
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2025-08-09 20:16:31 +0530

    Added cart functionality

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import capuchino from "../../assets/capuchino.jpeg";
 
-export default function ProductCard({ data = {}, increment, decrement }) {
+export default function ProductCard({
+  data = {},
+  increment,
+  decrement,
+  isAddedToCart = false,
+  handleAddToCart,
+  cartQuantity = 0,
+}) {
   return (
     <div className="product_card">
       <img className="product_image" src={data.image} />
       <p className="product_name">{data.name}</p>
-      <div className="counter_btn">
-        <button onClick={() => decrement(data.id)}>-</button>
-        <p>{data.qty || 0}</p>
-        <button onClick={() => increment(data.id)}>+</button>
-      </div>
+      {isAddedToCart ? (
+        <div className="counter_btn">
+          <button onClick={() => decrement(data.id)}>-</button>
+          <p>{cartQuantity || 0}</p>
+          <button onClick={() => increment(data.id)}>+</button>
+        </div>
+      ) : (
+        <button
+          className="add_to_cart_btn"
+          onClick={() => handleAddToCart(data.id)}
+        >
+          Add to Cart
+        </button>
+      )}
     </div>
   );
 }
